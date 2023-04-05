b56c4eff38b7e9a11ab333d03cf750fbbdd11b99
Nammy <54203371+Nambaseking01@users.noreply.github.com>
2023-04-05 17:52:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 // Initialize Firebase
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyBWBp147C5vJDwe3T-5L9ehXiP1n0lqLDc",
   authDomain: "vocabulary-project-a52a8.firebaseapp.com",
-  projectId: "vocabulary-project-a52a8",
-});
+  projectId: "vocabulary-project-a52a8"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
 // Get a reference to the "words" collection in Firestore
-const db = firebase.firestore();
 const wordsRef = db.collection("words");
+
+// Function to add a new word to the database
+function addWord() {
+  // Get the word and definition inputs
+  const wordInput = document.getElementById("new-word-input");
+  const definitionInput = document.getElementById("new-definition-input");
+  const word = wordInput.value;
+  const definition = definitionInput.value;
+
+  // Clear the input fields
+  wordInput.value = "";
+  definitionInput.value = "";
+
+  // Add the new word to the database
+  wordsRef.add({
+    word: word,
+    definition: definition
+  })
+  .then(() => {
+    console.log("Word added to database");
+  })
+  .catch((error) => {
+    console.error("Error adding word to database: ", error);
+  });
+}
 
 // Function to get a random word from the database and display its definition
 function getRandomWord() {
@@ -18,23 +45,8 @@ function getRandomWord() {
     // Display the word and its definition
     const wordDefinition = document.getElementById("word-definition");
     wordDefinition.textContent = `${randomWord.data().word}: ${randomWord.data().definition}`;
-  });
-}
-
-// Function to add a new word to the database
-function addWord() {
-  // Get the word and definition from the input fields
-  const wordInput = document.getElementById("new-word-input");
-  const definitionInput = document.getElementById("new-definition-input");
-  const word = wordInput.value;
-  const definition = definitionInput.value;
-  // Add the word and definition to the database
-  wordsRef.add({
-    word: word,
-    definition: definition,
-  }).then(() => {
-    // Clear the input fields
-    wordInput.value = "";
-    definitionInput.value = "";
+  })
+  .catch((error) => {
+    console.error("Error getting random word from database: ", error);
   });
 }
